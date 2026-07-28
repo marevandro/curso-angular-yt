308f6a991747d7223f9d22cad13f716b6075860a
marevandro <marevandro95@gmail.com>
2026-07-27 21:11:06 -0300

    teste1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './componets/first-component/first-component.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @NgModule({
   declarations: [AppComponent, FirstComponentComponent],
